a6814ce480fabcae359f05affa87ee827c2df821
michele laptop <micheledong2803@gmail.com>
2024-04-20 00:15:05 +0200

    bonus2

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-const ul = document.querySelector("ul.list");
+const row = document.querySelector("div.row");
 
 for(let i = 1; i<101 ; i++){
     if(i%3==0 && i%5==0){
-        const li = document.createElement("li");
-        li.append("FizzBuzz");
-        ul.append(li);
+        const col = document.createElement("div");
+        const div = document.createElement("div")
+        div.className = "fizzbuzz";
+        col.className="col-2";
+        div.append("FizzBuzz");
+        row.append(col);
+        col.append(div);
     }else if(i%3==0){
-        const li = document.createElement("li");
-        li.append("Fizz");
-        ul.append(li);
+        const col = document.createElement("div");
+        const div = document.createElement("div")
+        div.className = "fizz";
+        col.className="col-2";
+        div.append("Fizz");
+        row.append(col);
+        col.append(div);
     }else if(i%5==0){
-        const li = document.createElement("li");
-        li.append("Buzz");
-        ul.append(li);
+        const col = document.createElement("div");
+        const div = document.createElement("div")
+        div.className = "buzz";
+        col.className="col-2";
+        div.append("Buzz");
+        row.append(col);
+        col.append(div);
     }else{
-        const li = document.createElement("li");
-        li.append(i);
-        ul.append(li);
+        const col = document.createElement("div");
+        const div = document.createElement("div")
+        div.className = "number";
+        col.className="col-2";
+        div.append(i);
+        row.append(col);
+        col.append(div);
     }
 }
