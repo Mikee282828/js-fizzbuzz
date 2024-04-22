8769501606a2327315fccedc7310cc9ee86ce1bb
MikeDesktop <micheledong2803@gmail.com>
2024-04-22 11:19:07 +0200

    corretto

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
 const row = document.querySelector("div.row");
 
 for(let i = 1; i<101 ; i++){
+    let valore = i;
+    let classe = "number";
     if(i%3==0 && i%5==0){
-        const col = document.createElement("div");
-        const div = document.createElement("div")
-        div.className = "fizzbuzz";
-        col.className="col-2";
-        div.append("FizzBuzz");
-        row.append(col);
-        col.append(div);
+        valore = "FizzBuzz";
+        classe = "FizzBuzz";
     }else if(i%3==0){
-        const col = document.createElement("div");
-        const div = document.createElement("div")
-        div.className = "fizz";
-        col.className="col-2";
-        div.append("Fizz");
-        row.append(col);
-        col.append(div);
+        valore = "Fizz";
+        classe = "Fizz";
     }else if(i%5==0){
-        const col = document.createElement("div");
-        const div = document.createElement("div")
-        div.className = "buzz";
-        col.className="col-2";
-        div.append("Buzz");
-        row.append(col);
-        col.append(div);
-    }else{
-        const col = document.createElement("div");
-        const div = document.createElement("div")
-        div.className = "number";
-        col.className="col-2";
-        div.append(i);
-        row.append(col);
-        col.append(div);
+        valore = "Buzz";
+        classe = "Buzz";
     }
+    const col = document.createElement("div");
+    const div = document.createElement("div");
+    div.className = classe;
+    col.className="col-2";
+    div.append(valore);
+    row.append(col);
+    col.append(div);
 }
